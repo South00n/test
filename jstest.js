dc4d3629f7c2e24af2a93b1d243438ba977d48c0
Namhun Kim <nhk828@naver.com>
2022-09-02 01:20:32 +0900

    test

--- jstest.js
+++ jstest.js
@@ -3,3 +3,27 @@ var change = document.querySelector('#main');
     change.onclick = function() {
     change.style.color = "red";
   }
+  var change2 = document.querySelector('#main2');
+
+    change2.onclick = function() {
+    change2.style.color = "blue";
+  }
+
+  var change3 = document.querySelector('#main3');
+
+    change3.onclick = function() {
+    change3.style.color = "green";
+  }
+
+  var change4 = document.querySelector('#main4');
+
+    change4.onclick = function() {
+    change4.style.color = "pink";
+  }
+
+  var change5 = document.querySelector('#main5');
+
+    change5.onclick = function() {
+    change5.style.color = "tomato";
+  }
+
